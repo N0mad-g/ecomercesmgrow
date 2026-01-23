ca9599252e26da15e7fd721aa26872d0bf8184ae
N0mad-g <gustavo@gmail.com>
2026-01-23 12:14:08 -0300

    feat: add category product page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,4 +1,4 @@
-"use client";
+import Link from "next/link";
 
 import { categoryTable } from "@/db/schema";
 
@@ -18,7 +18,7 @@ const CategorySelector = ({ categories }: CategorySelectorProps) => {
             variant="ghost"
             className="rounded-full bg-white text-xs font-semibold"
           >
-            {category.name}
+            <Link href={`/category/${category.slug}`}>{category.name}</Link>
           </Button>
         ))}
       </div>
